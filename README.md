c03f0244ad2103e1f8e3f0ad1faeac5830878b6d
ashiqursuperfly <1605103@ugrad.cse.buet.ac.bd>
2019-06-14 00:42:00 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+##### run all commands in init.sh before starting an analysis.
+##### first convert your .c file to bitcode .bc file.
+##### now u can run all the analysis provided by SVF
---
+++ init.sh
@@ -0,0 +1,6 @@
+export LLVM_SRC=~/Workspaces/SVF/llvm-7.0.0.src
+export LLVM_OBJ=~/Workspaces/SVF/llvm-7.0.0.obj
+export LLVM_DIR=~/Workspaces/SVF/llvm-7.0.0.obj
+export PATH=$LLVM_DIR/bin:$PATH
+export SVF_HOME=~/Workspaces/SVF/SVF
+export PATH=$SVF_HOME/Release-build/bin:$PATH
